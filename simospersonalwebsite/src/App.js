449ba12f362a1d21fc829414481f41ffae3d1409
Simos Imbraem <simib98@hotmail.com>
2025-05-28 18:33:35 +0300

    initial commit for project

---
+++ simospersonalwebsite/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import LeftMenu from './LeftMenu'; 
+
+function App() {
+  return (
+    <LeftMenu />
+  );
+}
+
+export default App;
---
+++ simospersonalwebsite/src/LeftMenu.js
@@ -0,0 +1,9 @@
+function LeftMenu(){
+    return(
+        <div className="left-menu">
+            <p>Hello this is simos</p>
+        </div>
+    );
+}
+
+export default LeftMenu;
