3933505e0e32dc27d783bc58daa5762f74e00ccc
simosimb98 <simib98@hotmail.com>
2025-05-29 19:39:28 +0300

    Moving

--- simospersonalwebsite/src/components/Navbar.js
+++ simospersonalwebsite/src/components/Navbar.js
@@ -1,31 +1,39 @@
 import React, { useState } from 'react'; 
 import * as FaIcons from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { SideBarData } from './SideBarData';
+import './Navbar.css';
 
 function Navbar() {
 
     const [sidebar, setSidebar] = useState(false);
 
+    const showSidebar = () => {
+        setSidebar(!sidebar);
+    }
+
     return(
         <>
             <div className="navbar">
                 <Link to="/" className="navbar-link">
-                    <FaIcons.FaHome className="navbar-icon" />
+                    <FaIcons.FaHome className="navbar-icon" onClick={showSidebar} />
                 </Link>
-                <nav className={sidebar ? "navbar-menu active" : "navbar-menu"}>
-                    <ul className="navbar-menu-items">
-                        <li className="navbar-item">
-                            <Link to="/about" className="navbar-link">About</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/projects" className="navbar-link">Projects</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/contact" className="navbar-link">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
             </div>
+            
+            <nav className={sidebar ? "navbar-menu active" : "navbar-menu"}>
+                <ul className="navbar-menu-items">
+                    <li className="nav-text">
+                        <Link to="/"><FaIcons.FaRegWindowClose /> <span>Close</span></Link>
+                    </li>
+                    {SideBarData.map((itm,idx)=> {
+                        return(
+                            <li className={itm.clName} key={idx}>
+                                <Link to={itm.path}>{itm.icon} <span>{itm.title}</span></Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
         </>
     );
 }
---
+++ simospersonalwebsite/src/components/SideBarData.js
@@ -0,0 +1,16 @@
+import * as FaIcons from 'react-icons/fa';
+
+export const SideBarData = [
+    {
+        title : 'About me',
+        path  : '/about',
+        icon  : <FaIcons.FaRegGrin />,
+        clName: 'nav-text'
+    },
+    {
+        title : 'Contact',
+        path  : '/contact',
+        icon  : <FaIcons.FaRegEnvelopeOpen />,
+        clName: 'nav-text'
+    }
+]
