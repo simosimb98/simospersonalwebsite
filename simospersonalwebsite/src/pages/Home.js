f417ec6dbef31a189ed4810a857eeb891e9f92f0
simosimb98 <simib98@hotmail.com>
2025-06-01 15:22:51 +0300

    useNavigate

--- simospersonalwebsite/src/pages/Home.js
+++ simospersonalwebsite/src/pages/Home.js
@@ -1,20 +1,29 @@
-import { colors } from '@mui/material';
 import './Home.css';
-import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-    return(
-        <>
-            <div className="profile-picture">
-                <img width="300" height="300" src="/images/simos_imbraem_logo_1_new.png"/>
-            </div>
-            <div className="welcome-text">
-                <h4>Hello there!</h4>
-                <h1>I am<span> Simos Imbraem</span><br />a Full Stack Developer</h1>
-                <button className="home-about-button">GET TO KNOW ABOUT ME!</button>
-            </div>
-        </>
-    )
+  const navigateToAbout = useNavigate();
+
+  return (
+    <>
+      <div className="profile-picture">
+        <img width="300" height="300" src="/images/simos_imbraem_logo_1_new.png" alt="Simos Imbraem"/>
+      </div>
+      <div className="welcome-text">
+        <h4>Hello there!</h4>
+        <h1>
+          I am<span> Simos Imbraem</span><br />
+          a Full Stack Developer
+        </h1>
+        <button
+          className="home-about-button"
+          onClick={() => navigateToAbout('/about')}
+        >
+          GET TO KNOW ABOUT ME!
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
