8ac477752e38107fbaca30b081bf06c90731bfb2
simosimb98 <simib98@hotmail.com>
2025-06-08 16:56:56 +0300

    github api for repos

--- simospersonalwebsite/src/pages/Projects.js
+++ simospersonalwebsite/src/pages/Projects.js
@@ -1,17 +1,32 @@
 import { Element, Events, scrollSpy } from "react-scroll";
+import axios from "axios";
 import React, { useEffect, useState } from 'react';
 
-function Projects (owner, repo) {
-    const [repoData, setData] = useState(null);
+function Projects (props) {
+    const [repoData, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios
-            .get(`https://api.github.com/repos/${owner}/${repo}`)
-    });
+            .get(`https://api.github.com/users/${props.owner}/repos`)
+            .then(resp => {
+                setData(resp.data);
+                setLoading(true);
+            })
+             .catch(error => {
+                console.log(error);
+                setLoading(false);
+            });
+    }, [owner]);
 
     return (
         <Element name="projects" className="section">
-           
+           <div className="open-source-repositories">
+                {
+                    repoData.map(())
+                }
+
+           </div>
         </Element>
     );
 }
